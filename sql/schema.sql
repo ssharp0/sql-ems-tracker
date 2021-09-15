b4052a7346f3f6c25c20039b01bd6c42cc33f990
SHARP <ssharp9999@gmail.com>
2021-09-15 09:55:46 -0700

    schema

---
+++ sql/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS staff_db;
+
+CREATE DATABASE staff_db;
+
+USE staff_db;
+
+CREATE TABLE departments (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE roles (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  -- GAAP goes to 4 decimal places
+  salary DECIMAL(19,4) NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employees (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+  FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+);
